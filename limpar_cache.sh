29d52990070e6a160662818dbec0bb96d1be27ab
salyan-scripts <yanbeda94@gmail.com>
2026-07-26 02:09:08 -0300

    Update limpar_cache.sh
    limpar cache v3

--- limpar_cache.sh
+++ limpar_cache.sh
@@ -2,8 +2,7 @@
 sudo clear ;
 free -h ;
 sudo sync ;
-
-# Estas opções abaixo podem ser usadas em qualquer sistema linux, somente descomente a linha a ser usada.
+cat /proc/buddyinfo
 # Limpa a memória cache da RAM sem afetar os dados ativos
 #echo 1 | sudo tee /proc/sys/vm/drop_caches > /dev/null
 # Limpa dentries e inodes do cache da RAM sem prejudicar processos ativos
@@ -11,14 +10,16 @@ sudo sync ;
 # Limpa dentries, inodes e páginas de memória de arquivos inativos
 #echo 3 | sudo tee /proc/sys/vm/drop_caches > /dev/null
 
-# Estas opções abaixo ficam mais estáveis quando se usa o systemd, ou em alguma distribuição linux que use também o sysctl.
 # Limpa a memória cache da RAM sem afetar os dados ativos
 #sudo sysctl -w vm.drop_caches=1 ;
 # Limpa dentries e inodes do cache da RAM sem prejudicar processos ativos
 #sudo sysctl -w vm.drop_caches=2 ;
 # Limpa dentries, inodes e páginas de memória de arquivos inativos
-sudo sysctl -w vm.drop_caches=3 ;
+#sudo sysctl -w vm.drop_caches=3 ;
+
+echo 1 | sudo tee /proc/sys/vm/compact_memory > /dev/null;
+
+cat /proc/buddyinfo;
 
 sudo sync ;
-
 free -h ;
